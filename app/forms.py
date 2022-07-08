a2d247b5df6d4d6cbee2e5d0499328b6143c2b5e
ambane <austin.bane@gmail.com>
2022-07-08 18:29:08 -0400

    Add forms, routes, and templates to add, view, and edit clients

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, RadioField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, RadioField, FormField, DateField
 from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
 from app.models import User
 
@@ -27,3 +27,16 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please use a different email')
+
+
+class ClientInformationForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    middle_name = StringField('Middle Name')
+    dob = DateField('DOB', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    cell = StringField('Cell Phone')
+    home = StringField('Home Phone')
+    work = StringField('Work Phone')
+    submit = SubmitField('Add Client')
+
--- app/routes.py
+++ app/routes.py
@@ -2,8 +2,8 @@ from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
-from app.models import User
+from app.forms import LoginForm, RegistrationForm, ClientInformationForm
+from app.models import User, Client
 
 
 @app.route('/')
@@ -51,8 +51,59 @@ def register():
     return render_template('register.html', title='Register', form=form)
 
 
-@app.route('/user/<username>')
+@app.route('/user_profile/<username>')
 @login_required
-def user(username):
+def user_profile(username):
     user = User.query.filter_by(username=username).first_or_404()
     return render_template('user.html', title='User Profile', user=user)
+
+
+@app.route('/client_profile/<client_id>')
+@login_required
+def client_profile(client_id):
+    client = Client.query.get(int(client_id))
+    return render_template('client.html', title='Client Profile', client=client)
+
+
+@app.route('/add_client', methods=['GET', 'POST'])
+@login_required
+def add_client():
+    form = ClientInformationForm()
+    if form.validate_on_submit():
+        client = Client(first_name=form.first_name.data, last_name=form.last_name.data,
+                        middle_name=form.middle_name.data, dob=form.dob.data, email=form.email.data,
+                        cell_phone=form.cell_phone.data, work_phone=form.work_phone.data,
+                        home_phone=form.home_phone.data)
+        db.session.add(client)
+        db.session.commit()
+        return redirect(url_for('client', client_id=client.id))
+    return render_template('add_client.html', title='Add Client', form=form)
+
+
+@app.route('/edit_client/<client_id>', methods=['GET', 'POST'])
+@login_required
+def edit_client(client_id):
+    client = Client.query.get(int(client_id))
+    form = ClientInformationForm()
+    form.first_name = client.first_name
+    form.last_name = client.last_name
+    if client.middle_name is not None:
+        form.middle_name = client.middle_name
+    form.dob.data = client.dob
+    form.email.data = client.email
+    form.cell_phone.data = client.cell_phone
+    form.work_phone.data = client.work_phone
+    form.home_phone.data = client.home_phone
+    if form.validate_on_submit():
+        client.first_name = form.first_name.data
+        client.middle_name = form.middle_name.data
+        client.last_name = form.last_name.data
+        client.dob = form.dob.data
+        client.email = form.email.data
+        client.cell_phone = form.cell_phone.data
+        client.work_phone = form.work_phone.data
+        client.home_phone = form.home_phone.data
+        db.session.add(client)
+        db.session.commit()
+        return redirect(url_for('client.html', client_id=client_id))
+    return render_template('edit_client.html', title='Edit Client', form=form)
